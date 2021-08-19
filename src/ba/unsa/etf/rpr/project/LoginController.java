7bf3a62cd0f2101de191d003ac42128dfce9cf86
amnatrcalo <atrcalo1@etf.unsa.ba>
2021-08-19 13:16:09 +0200

    login page

--- src/ba/unsa/etf/rpr/project/LoginController.java
+++ src/ba/unsa/etf/rpr/project/LoginController.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr.project;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Control;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
+    public void hyperlinkAction(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/register.fxml"));
+        Stage registrationStage=new Stage();
+        registrationStage.setTitle("Registration");
+        registrationStage.setScene(new Scene(root, Control.USE_COMPUTED_SIZE, Control.USE_COMPUTED_SIZE));
+        registrationStage.setResizable(false);
+        registrationStage.show();
+    }
 }
