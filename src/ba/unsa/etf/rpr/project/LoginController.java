8deccfcd181360268ae4dbd7f252f9a1c1687563
amnatrcalo <atrcalo1@etf.unsa.ba>
2021-08-19 14:45:08 +0200

    add error-login message for empty username or password

--- src/ba/unsa/etf/rpr/project/LoginController.java
+++ src/ba/unsa/etf/rpr/project/LoginController.java
@@ -5,11 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Control;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginController {
+    public Label loginFailedField;
+    public TextField fldUsername;
+    public PasswordField fldPassword;
+
     public void hyperlinkAction(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/register.fxml"));
         Stage registrationStage=new Stage();
@@ -17,5 +24,16 @@ public class LoginController {
         registrationStage.setScene(new Scene(root, Control.USE_COMPUTED_SIZE, Control.USE_COMPUTED_SIZE));
         registrationStage.setResizable(false);
         registrationStage.show();
+    }
+
+
+    public void btnLoginAction(ActionEvent actionEvent) {
+        if(fldUsername.getText().trim().isEmpty() || fldPassword.getText().trim().isEmpty())
+        {
+            loginFailedField.setText("Login failed! Invalid username or password!");
+            loginFailedField.getStyleClass().add("loginFailed");
+        }
+
+
     }
 }
