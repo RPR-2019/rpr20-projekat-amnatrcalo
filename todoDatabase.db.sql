62ce2ca00c63d22aec300c0a20463fe68de93c93
amnatrcalo <atrcalo1@etf.unsa.ba>
2021-08-24 21:24:37 +0200

    add table lists

--- todoDatabase.db.sql
+++ todoDatabase.db.sql
@@ -36,14 +36,18 @@ CREATE TABLE IF NOT EXISTS "tasks" (
 	"list_name"	TEXT,
 	PRIMARY KEY("id")
 );
+CREATE TABLE IF NOT EXISTS "lists" (
+	"username"	TEXT,
+	"list_name"	TEXT,
+	PRIMARY KEY("username")
+);
 INSERT INTO "users" VALUES (0,'Amna','Trcalo','amna','amna');
 INSERT INTO "users" VALUES (1,'Mirna','Trcalo','mirna','mirna');
+INSERT INTO "users" VALUES (2,'maho','trcalo','maho','maho');
 INSERT INTO "quotes" VALUES (0,'A positive attitude causes a chain reaction of positive thoughts, events, and outcomes.','Wade Boggs');
-INSERT INTO "quotes" VALUES (1,'Every day that I wake up has to be a good day!
-','Bret Michaels');
-INSERT INTO "quotes" VALUES (2,'On a good day, when you have a clear plan, you are able to execute whatever you wanted.
-','Jasprit Bumrah');
-INSERT INTO "quotes" VALUES (3,'Anyone can have a good day, but you have to be able to perform on a bad day.
-','Jurgen Klopp');
+INSERT INTO "quotes" VALUES (1,'Every day that I wake up has to be a good day!','Bret Michaels');
+INSERT INTO "quotes" VALUES (2,'On a good day, when you have a clear plan, you are able to execute whatever you wanted.','Jasprit Bumrah');
+INSERT INTO "quotes" VALUES (3,'Anyone can have a good day, but you have to be able to perform on a bad day.','Jurgen Klopp');
 INSERT INTO "quotes" VALUES (4,' Don''t wake up with the regret of what you couldn''t accomplish yesterday. Wake up while thinking about what will be able to achieve today. ','Unknown');
+INSERT INTO "quotes" VALUES (5,'We don''t see things as they are, we see them as we are.','Anaïs Nin');
 COMMIT;
